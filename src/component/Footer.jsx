697a98bb761d49c5d504bbc3a81a02860c52b68a
GauravPachbhai <gauravpachbhai555@gmail.com>
2024-11-15 18:43:39 +0530

    changes

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -10,8 +10,8 @@ function Footer() {
                 <p className="flex gap-3"><Linkedin /><a href="https://www.linkedin.com/in/gaurav-pachbhai-526b39230/" target="_blank" rel="noopener noreferrer" className="text-blue-400">linkedin.com/in/gaurav-pachbhai/</a></p>
             </div>
             <div className="w-11/12 items-center text-blue-500 flex justify-center mt-8">
-                <div className=" w-1/5 md:w-1/4 border-t border-gray-500" />
-                <p className="text-center mx-4 text-sm sm:text-base">Copyright © 2022</p>
+                <div className=" w-11 md:w-1/4 border-t border-gray-500" />
+                <p className="text-center ml-2 text-sm sm:text-base">Copyright © 2024</p>
                 <div className="flex gap-4 p-2">
                  
                     <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
@@ -24,7 +24,7 @@ function Footer() {
                         <FaLinkedin />
                     </a>
                 </div>
-                <div className=" w-1/5 sm:w-1/4 border-t border-gray-500" />
+                <div className=" w-11 sm:w-1/4 border-t border-gray-500" />
             </div>
         </footer>
     );
